7af47146ea3a40ace4a33582a2470ce5fc2839a7
John Boyes <john@johnboyes.dev>
2024-07-07 17:27:48 -0400

    services padding

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,16 +5,13 @@ import services from '../data/services'
 export default function Services() {
   return (
     <div>
-        <div class="relative
-          
-        ">
-          {/* <img src="../../assets/meeting-lg.jpg"  class="w-full" /> */}
+        <div class="relative">
           <div class="text-overlay absolute left-auto top-auto p-6">
             IT Consulting Services
           </div>
         </div>
         <p className="text-xl font-bold text-left pb-8 ">Boyes Innovations</p>
-        <div className='md:mx-16 lg:mx-80'>
+        <div className='mx-8 md:mx-24 lg:mx-64 xl:mx-80'>
           {services.map(service => (
             <div className='mx-auto p-2 mb-2 bg-teal-200 rounded-lg shadow-md shadow-stone-400
             divide-y divide-stone-800'> 
