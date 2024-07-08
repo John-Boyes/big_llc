0fe7e8728c2598e50135c47d0d2f4e78c095f952
John Boyes <john@johnboyes.dev>
2024-07-07 20:43:26 -0400

    updated about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,6 +12,9 @@ export default class About extends Component {
            with a variety of IT-related projects. Whether you lack a dedicated IT team or your current team is occupied with other tasks,
             we are here to bridge the gap and ensure your IT projects are completed on time.
         </p>
+        <p>
+        Boyes Innovations Group is dedicated to leveraging agile principles to develop innovative solutions for the most complex technological challenges. Our expertise spans a wide range of services, whether you need to replace an outdated system or enhance your current infrastructure. We are committed to understanding your unique requirements and delivering custom solutions that are perfectly tailored to meet your needs. At Boyes Innovations Group, our goal is to transform your technological landscape, ensuring it is efficient, modern, and primed for future growth.
+        </p>
 
 
         </div>
