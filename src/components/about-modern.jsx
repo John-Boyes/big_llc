e6986c6f3c94498227681375c722faf979926812
John Boyes <john@johnboyes.dev>
2024-07-10 16:08:29 -0400

    updated css

--- src/components/about-modern.jsx
+++ src/components/about-modern.jsx
@@ -10,10 +10,10 @@ function About() {
                         {/* <p><div className='pl-4 pt-4 text-3xl'>Boyes Innovations Group</div></p> */}
                     </div>
                     
-                    <div class="anim-panel">
+                    <div class="animPanel">
                         {/* <p><div className='p-4 pl-8'>Professional IT Consulting Services</div></p> */}
                         <div className='m-10 pb-12'>
-                            <a href="/Contact" class="read-btn primary">Request a Consultation</a>
+                            <a href="/Contact" class="readBtn primary">Request a Consultation</a>
                         </div>
                     </div>
                 </div>
@@ -24,7 +24,7 @@ function About() {
                     <div class="clipper">
                         <p className='text-xl pb-4 lg:py-4'>About Us</p>
                     </div>
-                    <div class="anim-panel">
+                    <div class="animPanel">
                         <div class="filter">
                             <p className="font-semibold text-sm">
                             Founded in January 2021, Boyes Innovations Group specializes in supporting non-profits and small to medium-sized businesses
@@ -42,7 +42,7 @@ function About() {
                     <div class="clipper">
                         <p className='text-xl pb-4'>Our Mission</p>
                     </div>
-                    <div class="anim-panel">
+                    <div class="animPanel">
                     <p className="text-sm overflow-visible">
                             Boyes Innovations Group is dedicated to leveraging agile principles to develop innovative solutions for the most complex technological challenges. Our expertise spans a wide range of services, whether you need to replace an outdated system or enhance your current infrastructure. We are committed to understanding your unique requirements and delivering custom solutions that are perfectly tailored to meet your needs. At Boyes Innovations Group, our goal is to transform your technological landscape, ensuring it is efficient, modern, and primed for future growth.
                             </p>
