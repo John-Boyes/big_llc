4b9461b76681b308d68cf1ceaef276f809cc94f9
John Boyes <john@johnboyes.dev>
2024-10-06 12:27:49 -0400

    services formatting

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -11,9 +11,10 @@ export default function Services() {
   AOS.init();
 
   return (
-    <div className='flex flex-col scroll-p-8'>
+    <div className='flex flex-col scroll-p-8 min-h-screen'>
 
-        <div className='mx-6' data-aos="fade-up">
+        <div className='mx-6' 
+        data-aos="fade-up">
           {services.map(service => (
             <div className='mx-auto p-2 mb-2 bg-teal-200 rounded-lg shadow-md shadow-stone-400
             divide-y divide-stone-800'> 
